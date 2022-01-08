dbafab7e46d4a200fcccbfb4064f1ee528afea38
David L <davemlacroix@gmail.com>
2022-01-08 14:09:32 -0500

    added sudoku-cell component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SudokuCellComponent } from './sudoku-cell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SudokuCellComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/sudoku-cell.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'sudoku-cell',
+    template: `
+    <div class="sudoku-cell"> 9 </div>
+    `,
+    styleUrls: ['./sudoku-cell.component.css']
+})
+export class SudokuCellComponent{
+
+}
