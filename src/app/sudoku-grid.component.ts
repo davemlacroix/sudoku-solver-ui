adc2e8417d73962908f7dcc2620ce708655152aa
David L <davemlacroix@gmail.com>
2022-01-08 14:56:39 -0500

    created sudoku-grid component. removed excess divs from sudoku-cell and addressed height issues.

---
+++ src/app/sudoku-grid.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'sudoku-grid',
+    templateUrl: './sudoku-grid.component.html',
+    styleUrls: ['./sudoku-grid.component.css']
+})
+export class SudokuGridComponent{
+
+}
