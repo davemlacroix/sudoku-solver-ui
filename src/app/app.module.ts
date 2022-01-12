9cbc76c8289c5cd75543865783da89c5bf73652f
David L <davemlacroix@gmail.com>
2022-01-11 19:47:51 -0500

    remove sudoku-grid component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SudokuCellComponent } from './sudoku-cell.component';
-import { SudokuGridComponent } from './sudoku-grid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SudokuCellComponent,
-    SudokuGridComponent
+    SudokuCellComponent
   ],
   imports: [
     BrowserModule,
