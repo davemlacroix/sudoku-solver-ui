cbc97b35ab5e2dde5a620f56f9d5b2ec59b68543
davem <davemlacroix@gmail.com>
2022-01-13 17:10:41 -0500

    added sudoku-puzzle component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SudokuCellComponent } from './sudoku-cell.component';
+import { SudokuPuzzleComponent } from './sudoku-puzzle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SudokuCellComponent
+    SudokuCellComponent,
+    SudokuPuzzleComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/sudoku-puzzle.component.ts
@@ -0,0 +1,291 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector: 'sudoku-puzzle',
+    templateUrl: './sudoku-puzzle.component.html',
+    styleUrls: ['./sudoku-puzzle.component.css']
+})
+export class SudokuPuzzleComponent{
+
+puzzle = [
+    {
+    row: [
+        {
+        value: 8
+        },
+        {
+        value: 9
+        },
+        {
+        value: 7
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+    ]
+    },
+    {
+    row: [
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+    ]
+    },
+    {
+    row: [
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+    ]
+    },
+    {
+    row: [
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+    ]
+    },
+    {
+    row: [
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+    ]
+    },
+    {
+    row: [
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+    ]
+    },
+    {
+    row: [
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+    ]
+    },
+    {
+    row: [
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+    ]
+    },
+    {
+    row: [
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+        {
+        value: 9
+        },
+    ]
+    }
+]
+}
