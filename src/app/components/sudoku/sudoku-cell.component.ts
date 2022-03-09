90f3f5e1cf57f3de2b36148882ab3367828eec38
davem <davemlacroix@gmail.com>
2022-03-08 22:06:41 -0500

    reformat cells

--- src/app/components/sudoku/sudoku-cell.component.ts
+++ src/app/components/sudoku/sudoku-cell.component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from "@angular/core";
 
 @Component({
     selector: 'sudoku-cell',
-    template: `{{value}}`
+    template: `<div class="cell"><span class="cellText">{{value}}</span></div>`,
+    styleUrls: ['./sudoku-cell.component.css']
 })
 export class SudokuCellComponent{
     @Input() value:any
