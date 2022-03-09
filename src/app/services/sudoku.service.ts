58011aa3603f6983c3cab4a7b8d9933839cda3a9
davem <davemlacroix@gmail.com>
2022-03-08 21:08:39 -0500

    use api call to get puzzle data

--- src/app/services/sudoku.service.ts
+++ src/app/services/sudoku.service.ts
@@ -1,20 +1,27 @@
 import { Injectable } from "@angular/core"
 import { HttpClient } from '@angular/common/http'
-import { Observable, Subject } from "rxjs"
+import { Observable, Subject, of, catchError } from "rxjs"
 import { ISudokuPuzzle } from "../models/sudoku-puzzle.model"
 import { ISudokuCell } from "../models/sudoku-cell.model";
 
 @Injectable()
 export class SudokuService  {
-    constructor(private http: HttpClient)
+    constructor(private httpClient: HttpClient)
     {
 
     }
 
     getSudokuPuzzle():Observable<ISudokuPuzzle> { 
-        let subject = new Subject<ISudokuPuzzle>();
-        setTimeout(() => {subject.next(puzzle); subject.complete();}, 100);
-        return subject;
+        return this.httpClient.get<ISudokuPuzzle>('https:/localhost:44379/api/puzzle')
+            .pipe(catchError(this.handleError<ISudokuPuzzle>('getSudokuPuzzle', puzzle)));
+
+    }
+
+    private handleError<T> (operation = 'operation', result?:T) {
+        return (error: any): Observable<T> => {
+            console.error(error);
+            return of(result as T);
+        }
     }
 }
 
